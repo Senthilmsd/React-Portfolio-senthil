d76f4d12d57ed6fd55a13aeffcb822a9cc559775
S S Senthil Kumar <senthilmsd2003@gmail.com>
2025-02-06 13:14:06 +0530

    Added Student Form and To-Do List projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,5 +1,7 @@
 import project1 from '../assets/projects/project-img.png'
 import project2 from '../assets/projects/project-portfolio.png'
+import project3 from '../assets/projects/project-Std-Form.png'
+import project4 from '../assets/projects/project-TO-DO-List.png'
 import {motion} from "framer-motion"
 
 const Project =()=>{
@@ -36,6 +38,10 @@ const Project =()=>{
                             <span key={value} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{value}</span>
 
                         ))}
+
+<div className='inline-flex px-6 py-3  bg-neutral-900 text-black text-lg font-medium rounded-md shadow-md border border-gray-400 hover:bg-gray-200 transition duration-300 no-underline'>
+                        <a href="https://react-portfolio-senthil.vercel.app/">Visit my App</a>  
+                        </div>
                     </motion.div>
                 </div>
                 <div className="mb-8 flex flex-wrap lg:justify-center">
@@ -55,7 +61,71 @@ const Project =()=>{
                         <p className='mb-4 text-neutral-400 xs:w-72 xs:ml-2'>Developed a Java-based bus booking system leveraging loops and conditional statements. Built a userfriendly interface and functional logic for managing bookings.
                         </p>
                         <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">JAVA</span>
+                        
+                        
                     </motion.div>
+                    
+                </div>
+
+                <div className="mb-8 flex flex-wrap lg:justify-center">
+                    <motion.div
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1.5}}
+                    className="w-full lg:w-1/4">
+                        <img src={project3} alt="Project-1" width={150} height={150} className='mt-6 rounded xs:ml-20'/>
+                    </motion.div>
+                    <motion.div
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: 100}}
+                    transition={{duration: 1.5}}
+                    className="w-full max-w-xl lg:w-4/4">
+                        <h4 className='mb-2 font-semibold  xs:ml-2'>Student Form Management</h4>
+                        <p className='mb-4 text-neutral-400 xs:w-72 xs:ml-2'>A simple yet efficient CRUD application built with HTML, CSS, and JavaScript. It allows users to create, read, update, and delete student records with a clean and responsive interface, showcasing my front-end development skills.
+                        </p>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">HTML</span>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">CSS</span>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">JAVASCRIPT</span>
+                        <div className='inline-flex px-6 py-3  bg-neutral-900 text-black text-lg font-medium rounded-md shadow-md border border-gray-400 hover:bg-gray-200 transition duration-300 no-underline'>
+                        <a href="https://student-form-ashen.vercel.app/">Visit my App</a>  
+                        </div>
+                    </motion.div>
+                    
+                </div>
+
+                <div className="mb-8 flex flex-wrap lg:justify-center">
+                    <motion.div
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1.5}}
+                    className="w-full lg:w-1/4">
+                        <img src={project4} alt="Project-1" width={150} height={150} className='mt-6 rounded xs:ml-20'/>
+                    </motion.div>
+                    <motion.div
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: 100}}
+                    transition={{duration: 1.5}}
+                    className="w-full max-w-xl lg:w-4/4">
+                        <h4 className='mb-2 font-semibold  xs:ml-2'>To-Do List App</h4>
+                        <p className='mb-4 text-neutral-400 xs:w-72 xs:ml-2'>A sleek and interactive To-Do List built with HTML, JavaScript, and Tailwind CSS. It allows users to add tasks, mark them as completed with a click, and delete them effortlessly. With a minimal and responsive design, this project highlights my skills in DOM manipulation and UI development.
+                        </p>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">HTML</span>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">TAILWIND CSS</span>
+                        <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">JAVASCRIPT</span>
+                        <div className='inline-flex px-6 py-3  bg-neutral-900 text-black text-lg font-medium rounded-md shadow-md border border-gray-400 hover:bg-gray-200 transition duration-300 no-underline'>
+                        <a href="https://to-do-list-using-js-sigma.vercel.app/">Visit my App</a>  
+                        </div>
+                    </motion.div>
+
+                    {/* <motion.div
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1.5}}
+                    className="w-full lg:w-1/4">
+                        {/* <img src={project4} alt="Project-1" width={150} height={150} className='mt-6 rounded xs:ml-20'/> */}
+                        
+                    {/* </motion.div> */} 
+                    
                 </div>
             </div>
         </div>
